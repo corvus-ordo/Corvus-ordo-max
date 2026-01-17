58704495cc6e488b09c47cf91d0f49dc793575ac
corvus-ordo <corvus.ordo.contact@proton.me>
2026-01-17 21:12:47 +0100

    Update index.js

--- core/db/index.js
+++ core/db/index.js
@@ -2,24 +2,36 @@
 import { Sequelize } from 'sequelize';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const sequelize = new Sequelize(process.env.DB_URL, {
-  logging: false,
+  logging: false
 });
 
 const db = {};
 
-fs.readdirSync(path.join(__dirname, '../models')).forEach(file => {
-  const modelDef = require(`../models/${file}`).default;
+const modelsPath = path.join(__dirname, '../models');
+const files = fs.readdirSync(modelsPath).filter(f => f.endsWith('.js'));
+
+for (const file of files) {
+  const fullPath = path.join(modelsPath, file);
+  const mod = await import(fullPath);
+  const modelDef = mod.default;
   const model = modelDef(sequelize, Sequelize.DataTypes);
   db[model.name] = model;
-});
+}
 
 Object.values(db).forEach(model => {
-  if (model.associate) model.associate(db);
+  if (typeof model.associate === 'function') {
+    model.associate(db);
+  }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 export default db;
+

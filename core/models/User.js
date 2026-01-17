e036da3a0dcfb21d87b65d3238217fc20383df2e
corvus-ordo <corvus.ordo.contact@proton.me>
2026-01-17 16:56:53 +0100

    Update User.js

--- core/models/User.js
+++ core/models/User.js
@@ -1,1 +1,17 @@
+// /core/models/User.js
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    passwordHash: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'user' },
+    isActive: { type: DataTypes.BOOLEAN, defaultValue: true }
+  });
+
+  User.associate = models => {
+    User.hasMany(models.Order, { foreignKey: 'userId' });
+  };
+
+  return User;
+};
 

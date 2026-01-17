0e0b46ae4ff92ea8c2b462f72a346dfa5ccfda07
corvus-ordo <corvus.ordo.contact@proton.me>
2026-01-17 20:59:07 +0100

    Create Delivery.js

---
+++ core/models/Delivery.js
@@ -0,0 +1,32 @@
+// /core/models/Delivery.js
+export default (sequelize, DataTypes) => {
+  const Delivery = sequelize.define('Delivery', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false // np. "Kurier DPD", "InPost Paczkomat"
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    estimatedDays: {
+      type: DataTypes.INTEGER,
+      allowNull: false // np. 1–3 dni
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  });
+
+  Delivery.associate = models => {
+    Delivery.hasMany(models.Order, { foreignKey: 'deliveryId' });
+  };
+
+  return Delivery;
+};

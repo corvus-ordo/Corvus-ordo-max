8aa0ee39e1de9577ce2d5dd8e16732db259a2056
corvus-ordo <corvus.ordo.contact@proton.me>
2026-01-18 11:34:05 +0100

    Update commentController.js

--- core/controllers/commentController.js
+++ core/controllers/commentController.js
@@ -1,1 +1,34 @@
+// /core/controllers/commentController.js
+import * as commentService from '../services/commentService.js';
+
+export async function addComment(req, res, next) {
+  try {
+    const comment = await commentService.addComment({
+      userId: req.user.id,
+      publicationId: req.params.pubId,
+      content: req.body.content
+    });
+    res.status(201).json(comment);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function removeComment(req, res, next) {
+  try {
+    await commentService.deleteComment(req.params.id, req.user.id);
+    res.json({ message: 'Comment deleted' });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getPublicationComments(req, res, next) {
+  try {
+    const comments = await commentService.getComments(req.params.pubId);
+    res.json(comments);
+  } catch (err) {
+    next(err);
+  }
+}
 

33241fa78f080f27b829fef78b158685e26bfeeb
corvus-ordo <corvus.ordo.contact@proton.me>
2026-01-18 18:08:29 +0100

    Update auditController.js

--- core/controllers/auditController.js
+++ core/controllers/auditController.js
@@ -9,3 +9,17 @@ export async function getAuditLogs(req, res, next) {
     next(err);
   }
 }
+
+import db from '../db/index.js';
+
+export async function getAuditLogs(req, res, next) {
+  try {
+    const logs = await db.AuditLog.findAll({
+      order: [['createdAt', 'DESC']],
+      limit: 200
+    });
+    res.json(logs);
+  } catch (err) {
+    next(err);
+  }
+}
